790a885b6292d1a0c8284f14d261e4e7e1b0dc3c
Vu Gia Huy <GiaHuy300400@gmail.com>
2023-02-07 14:29:47 +0700

    post

---
+++ Models/Blog/PostCategory.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+namespace App.Models.Blog;
+[Table("PostCategory")]
+public class PostCategory
+{
+    public int PostId {get;set;}
+    public int CategoryId {get;set;}
+
+    [ForeignKey("PostId")]
+    public Post Post{get;set;}
+    
+    [ForeignKey("CategoryId")]
+    public Category Category {get;set;}
+}
